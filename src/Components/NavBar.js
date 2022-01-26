d7b79e047493b0ddc78cbf02c21144dc6a853418
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-01-25 21:26:27 -0500

    modify some files

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,7 +6,6 @@ export default function NavBar() {
       <h1 id="budgetapp">
         <Link to="/transactions">Budget App</Link>
       </h1>
-      {/* <h2 id="bankAcountTotal" className={total>1000? "green":total>0? "grey":"red"}>Bank Account Total: ${total}</h2> */}
       <button>
         <Link to="transactions/new">NEW TRANSACTION</Link>
       </button>
